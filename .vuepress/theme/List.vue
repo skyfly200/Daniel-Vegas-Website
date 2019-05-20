7a578434d242d55dca9232d00499ec2f9abfa9ae
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-05-20 14:03:44 -0600

    update list of works theme

--- .vuepress/theme/List.vue
+++ .vuepress/theme/List.vue
@@ -7,7 +7,27 @@
           <h1>{{ $page.title }}</h1>
         </v-layout>
         <v-layout class="pa-4">
-          <Content />
+          <v-flex xs12 md10 offset-md1>
+            <Content />
+          </v-flex>
+        </v-layout>
+        <v-layout class="pa-4">
+          <v-flex xs12 md10 offset-md1>
+            <v-card class="pa-4">
+              <ul>
+                <template v-for="i in $page.frontmatter.list">
+                  <li :key="i.title">
+                    <h3>
+                      <a v-if="i.type === 'work'" to="#">{{ i.work }}</a>
+                      <a v-if="i.type === 'external'" href="i.url">{{ i.title }}</a>
+                      <template v-else>{{ i.title }}</template>
+                    </h3>
+                    <p v-if="i.description">{{ i.description }}</p>
+                  </li>
+                </template>
+              </ul>
+            </v-card>
+          </v-flex>
         </v-layout>
       </v-container>
     </v-content>
--- list.md
+++ list.md
@@ -1,8 +1,17 @@
 ---
 pageClass: "list"
 title: "List of Works"
+list:
+  - title: No Link
+    type: none
+    description: you can have a short post description here
+  - type: work
+    work: Just a Stream
+    description: you can have a short post description here
+  - title: External Link
+    type: external
+    url: http://206.189.237.110:8000/KWHR
+    description: you can have a short post description here
 ---
 
-- item one
-- item two
-- more items
+Here is a list of works
