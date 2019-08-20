d3005dae0c76b6f0fc0a53c34ef947eaa49266ac
Skyler Fly-Wilson <skyfly200@gmail.com>
2019-08-19 22:52:22 -0600

    fix vuetify style

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
         }
     },
     head: [
-        ['link', { rel: 'stylesheet', href: `https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css` }],
+        ['link', { rel: 'stylesheet', href: `https://cdn.jsdelivr.net/npm/vuetify@1.x/dist/vuetify.min.css` }],
         ['link', { rel: 'stylesheet', href: `https://fonts.googleapis.com/icon?family=Material+Icons` }],
         ['link', { rel: 'icon', href: `/logo.png` }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
